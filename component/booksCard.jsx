d0ab75d60841aa6c64df72ce9b78cb6a6c64754e
Maria Gonzalez <chelo140581@gmail.com>
2022-05-31 16:10:49 +0200

    fix size from covers

--- component/booksCard.jsx
+++ component/booksCard.jsx
@@ -18,11 +18,13 @@ export default function BooksCard({ apikey }) {
         fetchedData.results.books.map(book => {
           return (
             <StyledCard key={book.book_uri}>
-              <article>
+              <StyledBookCover>
                 <Styledimage src={book.book_image} />
+              </StyledBookCover>
+              <div>
                 <h3>{book.title}</h3>
                 <p>{book.author}</p>
-              </article>
+              </div>
             </StyledCard>
           );
         })
@@ -33,22 +35,31 @@ export default function BooksCard({ apikey }) {
   );
 }
 
+const StyledBookCover = styled.div`
+  position: relative;
+  width: 100%;
+  background-color: red;
+  padding-bottom: 150%;
+`;
+
 const StyledCard = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   background-color: #bbd1e1;
   padding: 20px;
-  text-align: justify;
   border-radius: 25px;
 `;
 
 const CardWrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-gap: 20px;
+  padding: 0 20px;
 `;
 
 const Styledimage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
