7dc4c338fa8451da419d9df96df6eac93586f634
Maria Gonzalez <chelo140581@gmail.com>
2022-06-09 15:37:52 +0200

    add hydration hook

--- hooks/useStore.js
+++ hooks/useStore.js
@@ -1,34 +1,44 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useStore = create(set => ({
-  fetchedData: { results: [] },
-  fetchApi: async url => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      set({ fetchedData: data });
-    } catch (error) {
-      console.error(`Error: ${error}`);
+const useStore = create(
+  persist(
+    set => ({
+      fetchedData: { results: [] },
+      fetchApi: async url => {
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          set({ fetchedData: data });
+        } catch (error) {
+          console.error(`Error: ${error}`);
+        }
+      },
+
+      wishList: ['9781250278197', '123456', '9781501133572'],
+
+      addToWishList: isbn => {
+        set(state => {
+          return {
+            wishList: [...state.wishList, isbn],
+          };
+        });
+        console.log(isbn);
+      },
+      deleteFromWishList: isbn => {
+        set(state => {
+          return {
+            wishList: state.wishList.filter(item => item !== isbn),
+          };
+        });
+        console.log(isbn);
+      },
+    }),
+    {
+      name: 'book-storage', // name of item in the storage (must be unique)
+      getStorage: () => localStorage, // (optional) by default the 'localStorage' is used
     }
-  },
-
-  wishList: ['9781250278197', '123456', '9781501133572'],
-
-  addToWishList: isbn => {
-    set(state => {
-      return {
-        wishList: [...state.wishList, isbn],
-      };
-    });
-  },
-  deleteFromWishList: isbn => {
-    set(state => {
-      return {
-        wishList: state.wishList.filter(item => item !== isbn),
-      };
-    });
-    console.log(isbn);
-  },
-}));
+  )
+);
 
 export default useStore;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import { ThemeProvider } from 'styled-components';
 import '../styles/globals.css';
 import { createGlobalStyle } from 'styled-components';
+import useHydration from '../hooks/useHydration';
+import Pre from '../component/Pre';
 
 function MyApp({ Component, pageProps }) {
+  const hydrated = useHydration();
+  if (hydrated === false) {
+    return;
+    <></>;
+  }
+
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />;
+      <Pre />
     </>
   );
 }
