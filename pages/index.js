311b13dddbf78493b9ad49d8ed04064f03549a72
Maria Gonzalez <chelo140581@gmail.com>
2022-06-09 16:07:24 +0200

    delete component bookmark and component searchbar

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
 import BooksCard from '../component/booksCard';
-import { SearchBar } from '../component/searchBar';
-import { useState } from 'react';
 
 export default function Home() {
-  const [searchUrl, setSearchUrl] = useState('');
-
-  console.log(searchUrl);
-  const handleSubmit = inputValue => {
-    setSearchUrl(inputValue);
-  };
-
   return (
     <>
       <h1>My Book App </h1>
-      <SearchBar onSubmit={handleSubmit} />
+
       <BooksCard />
     </>
   );
