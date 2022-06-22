dc257c443e6b10e6d3a671ebd162a7f9cb32eb41
Maria Gonzalez <chelo140581@gmail.com>
2022-06-22 10:05:21 +0200

    change margin

--- pages/_app.js
+++ pages/_app.js
@@ -46,7 +46,7 @@ a {
 
 
   body {
-    margin: 0px 0px 70px 0;
+    margin: 0px 0px 90px 0;
     padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
